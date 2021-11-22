8373d6fe0d34eec8f46065e1fadd901384ca74ab
Mihailob5017 <mihailob5017@gmail.com>
2021-11-22 14:52:14 +0100

    set up react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Components
+import Homepage from './components/home/home.component';
+
+// Code
 const App = () => {
-	return <div>Hello World</div>;
+	return (
+		<BrowserRouter>
+			<div className='container'>
+				<Routes>
+					<Route path='/' exact element={<Homepage />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
---
+++ src/components/home/home.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+	return (
+		<>
+			<h1 className='homepage-header'>All Users</h1>
+		</>
+	);
+};
+
+export default Homepage;
