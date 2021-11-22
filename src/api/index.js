eb16de47bc57cb589010694857ea57791d86eff6
Mihailob5017 <mihailob5017@gmail.com>
2021-11-22 15:14:53 +0100

    set up the base api

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// Base Setup
+const BASE_URL = 'https://dummyapi.io/data/v1';
+axios.defaults.headers['app-id'] = '619ba2328213396838ca56c4';
+
+// Get all Users
+export const getAlUsers = () => axios.get(`${BASE_URL}/user`);
