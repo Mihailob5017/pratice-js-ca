35062c8a64031ac9d296dd041551d107224743ac
Mihailob5017 <mihailob5017@gmail.com>
2021-11-23 11:57:50 +0100

    created the single post

--- src/components/profile/profile.component.jsx
+++ src/components/profile/profile.component.jsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './profile.style.css';
+import LikeSvg from '../../svg/like.svg';
+
+// Moment
+import moment from 'moment';
 
 // React Router
 import { useLocation } from 'react-router-dom';
 
 // Redux
 import { useDispatch, connect } from 'react-redux';
-import {} from '../../redux/action.creators';
+import { getSpecificPost } from '../../redux/action.creators';
 
 // Components
 import LoadingComponent from '../loading/loading.component';
 
 // Code
-const Profile = () => {
+const Profile = ({ post, comments }) => {
 	// Getting the ID
 	let location = useLocation();
 	const id = location.pathname.split('/')[1];
@@ -20,9 +24,51 @@ const Profile = () => {
 	const [isLoading, setLoading] = useState(true);
 	const dispatch = useDispatch();
 
-	useEffect(() => {}, []);
+	useEffect(async () => {
+		await dispatch(getSpecificPost(id));
+		setLoading(false);
+	}, []);
 
-	return <div>ey</div>;
+	return (
+		<>
+			<div className='post-wrapper'>
+				{isLoading ? (
+					<LoadingComponent />
+				) : (
+					<>
+						<div className='post-container'>
+							<img src={post.image} alt='' />
+							<p className='single-post-title'>
+								<label>
+									{post.owner.firstName + ' ' + post.owner.lastName}
+								</label>
+								{' ' + post.text}
+							</p>
+							<div className='single-post-info'>
+								<div>
+									{post.likes} <img src={LikeSvg} alt='Like SVG' />
+									{post.tags.map((el, i) => (
+										<label key={i} className='tag'>
+											{el}
+										</label>
+									))}
+								</div>
+								<p>{moment(post.publishDate).format('MMMM Do YYYY')}</p>
+							</div>
+						</div>
+					</>
+				)}
+			</div>
+			<div className='comments-container'>
+				{isLoading ? <LoadingComponent /> : <>loaded</>}
+			</div>
+		</>
+	);
 };
 
-export default Profile;
+const mapStateToProps = (state) => ({
+	post: state.extendedPost.post,
+	comments: state.extendedPost.comments,
+});
+
+export default connect(mapStateToProps)(Profile);
